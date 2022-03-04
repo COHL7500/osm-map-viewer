ff1ac132b3bcbab7106bb171104e4c5c26af991f
Weihao Chen Nyholm-Andersen <weny@itu.dk>
2022-03-04 12:16:56 +0100

    Delete BFST22weny-handin3 directory

--- BFST22weny-handin3/app/src/main/java/bfst22/vector/Main.java
+++
@@ -1,9 +0,0 @@
-package bfst22.vector;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(App.class, args);
-    }
-}
