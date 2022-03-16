c0125e4fe5c5b85dfbf8e868ae8235200faadc10
ItzWeihao <weny@itu.dk>
2022-03-16 20:15:17 +0100

    Change to HBox
    Updated so the HBox has padding so it doesn't align uptowards the edges of the window

--- Map of Denmark/app/src/main/java/bfst22/vector/Controller.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/Controller.java
@@ -1,11 +1,10 @@
 package bfst22.vector;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 
@@ -14,17 +13,27 @@ import java.util.Arrays;
 // Responsible for controlling/updating the current view and manipulating dataflow of model.
 public class Controller {
     private Point2D lastMouse;
-    @FXML private MapCanvas canvas;
+
+    @FXML
+    private MapCanvas canvas;
+    @FXML
+    private MenuButton dropdown;
+    @FXML
+    private Slider slider;
 
     // Runs upon start of program: Initializes our MapCanvas based on model.
     public void init(final Model model) {
         this.canvas.init(model);
     }
 
-    // Handles an event of scrolling and increases/decreases the zoom level of the map.
-    // Default canvas zoom: 1.10
-    @FXML private void onScroll(final ScrollEvent e) {
-        this.canvas.zoom(Math.pow(1.003, e.getDeltaY()), e.getX(), e.getY()); // Change value to control sensitivity.
+    // Handles an event of scrolling and increases/decreases the zoom level of the
+    // map.
+    @FXML
+    private void onScroll(ScrollEvent e) {
+        var factor = e.getDeltaY();
+
+        // Default canvas zoom: 1.10
+        canvas.zoom(Math.pow(1.003, factor), e.getX(), e.getY()); // Change value to control sensitivity.
     }
 
     // Handles panning in the program
