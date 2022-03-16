7cb2d3842e14b0cca3adbcdf9d057277c5be7527
ItzWeihao <weny@itu.dk>
2022-03-16 16:22:13 +0100

    Changes to Visual Appearance
    Updated top bar to center and be adjustable to windows size
    Updated so the window has a minimum width and height
    Updates so the window will always start at a specific point on the screen

--- Map of Denmark/app/src/main/java/bfst22/vector/View.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/View.java
@@ -2,17 +2,34 @@ package bfst22.vector;
 
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 // Responsible for displaying model data.
 public class View {
     public View(Model model, Stage primaryStage) throws IOException {
-        primaryStage.show();
-        var loader = new FXMLLoader(View.class.getResource("View.fxml"));
-        primaryStage.setWidth(Screen.getPrimary().getBounds().getWidth()/1.5);
-        primaryStage.setHeight(Screen.getPrimary().getBounds().getHeight()/1.5);
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 600);
+        
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+        primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight() / 2));
+
+        
+        var loader = new FXMLLoader(View.class.getResource("ViewNew.fxml"));
         primaryStage.setScene(loader.load());
+
+        primaryStage.setWidth(800);
+        primaryStage.setMinWidth(400);
+        primaryStage.setHeight(600);
+        primaryStage.setMinHeight(300);
+        
+        primaryStage.show();
+
         Controller controller = loader.getController();
         controller.init(model);
         primaryStage.setTitle("Danmarkskort - Gruppe #1");
