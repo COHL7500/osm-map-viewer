824017207c94e1464f79aa839bf0442088469979
ItzWeihao <weny@itu.dk>
2022-03-16 20:15:17 +0100

    Change to HBox
    Updated so the HBox has padding so it doesn't align uptowards the edges of the window

--- Map of Denmark/app/src/main/java/bfst22/vector/View.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/View.java
@@ -3,35 +3,30 @@ package bfst22.vector;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 // Responsible for displaying model data.
 public class View {
     public View(Model model, Stage primaryStage) throws IOException {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 800, 600);
-        
         Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        double width = 800, height = 600;
 
         primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
         primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight() / 2));
 
-        
         var loader = new FXMLLoader(View.class.getResource("ViewNew.fxml"));
         primaryStage.setScene(loader.load());
 
-        primaryStage.setWidth(800);
-        primaryStage.setMinWidth(400);
-        primaryStage.setHeight(600);
-        primaryStage.setMinHeight(300);
-        
+        primaryStage.setWidth(width);
+        primaryStage.setMinWidth(width / 2);
+        primaryStage.setHeight(height);
+        primaryStage.setMinHeight(height / 2);
+
         primaryStage.show();
 
         Controller controller = loader.getController();
         controller.init(model);
-        primaryStage.setTitle("Danmarkskort - Gruppe #1");
+        primaryStage.setTitle("Map Of Denmark");
     }
 }
