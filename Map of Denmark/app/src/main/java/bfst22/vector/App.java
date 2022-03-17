0f785d4fd8c41053425050546030593ae59cc919
COHL7500 <cohl7500@gmail.com>
2022-03-17 15:30:31 +0100

    Major update in our wayconfig.yaml file

--- Map of Denmark/app/src/main/java/bfst22/vector/App.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/App.java
@@ -2,10 +2,18 @@ package bfst22.vector;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.Screen;
 
 // Responsible for initiating the application; Model is instantiated with our OSM file and finally View.
 public class App extends Application {
     @Override public void start(Stage primaryStage) throws Exception {
+
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+
+        primaryStage.setHeight(screenHeight / 1.5);
+        primaryStage.setWidth(screenWidth / 1.5);
+
         Model model = new Model("data/small.osm.zip"); // Reads OSM file from data folder.
         new View(model, primaryStage); // primaryStage is our main window
     }
