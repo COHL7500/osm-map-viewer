e801a4f61d079a26c6859843589eab3f44d72560
ItzWeihao <weny@itu.dk>
2022-03-25 13:01:29 +0100

    Updated GUI with few improvements

--- Map of Denmark/app/src/main/java/bfst22/vector/View.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/View.java
@@ -2,43 +2,62 @@ package bfst22.vector;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 // Responsible for displaying model data.
 public class View {
+
+    @FXML
+    private BorderPane someBorderPane;
+
+    // Main method for displaying the program
     public View(Model model, Stage primaryStage) throws IOException {
-        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
-        double width = 800, height = 600;
 
-        primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
-        primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight() / 2));
+        setDisplayBound(primaryStage);
 
-        var loader = new FXMLLoader(View.class.getResource("ViewNew.fxml"));
+        var loader = new FXMLLoader(View.class.getResource("ViewDemo.fxml"));
 
         Scene scene = loader.load();
-
-        primaryStage.setScene(scene);
-
-        primaryStage.setWidth(width);
-        primaryStage.setMinWidth(width / 2);
-        primaryStage.setHeight(height);
-        primaryStage.setMinHeight(height / 2);
-
-        primaryStage.show();
-
         scene.getStylesheets().clear();
 
-        // scene.getStylesheets().add("Map of Denmark/app/src/main/css/someStyling.css");
+        getCSS(scene);
 
+        setPrimaryStageSize(primaryStage);
 
-        // scene.getStylesheets().add(View.class.getResource("Map Of Denmark/app/src/main/resources/bfst22/vector/css/someStyling.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
         Controller controller = loader.getController();
         controller.init(model);
         primaryStage.setTitle("Map Of Denmark");
+    }
+
+    // Getting the CSS file and implement it on the scene
+    public void getCSS(Scene scene){
+        String css = View.class.getResource("someStyling.css").toExternalForm();
+        scene.getStylesheets().add(css);
+    }
+
+    // Setting the stage size for the application
+    public void setPrimaryStageSize(Stage primaryStage){
+        double width = 800, height = 600;
+        primaryStage.setWidth(width);
+        primaryStage.setMinWidth(width / 2);
+        primaryStage.setHeight(height);
+        primaryStage.setMinHeight(height / 2);
+    }
+
+    // Setting the window displaybound so the scene spawns within the screen
+    public void setDisplayBound(Stage primaryStage){
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+        primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight() / 2));
     }
 }
