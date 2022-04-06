69a70f889410b3dc7c7a18f324096bbaf94387f1
Adam Hadou Temsamani <ahad@itu.dk>
2022-04-06 11:22:06 +0200

    Required Commit

--- Map of Denmark/app/src/main/java/bfst22/vector/Distance.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/Distance.java
@@ -2,6 +2,7 @@ package bfst22.vector;
 
 
 public class Distance {
+
     Coordinates coordinates;
     /*
     Double lat1 = coordinates.getMinX();
@@ -12,19 +13,40 @@ public class Distance {
 
     final int R = 6371;
 
-    public Double haversineFormula(Double lat1, Double lon1, Double lat2, Double lon2){
-        Double DeltaLat = toRadians(lat1 - lat2);
-        Double DeltaLon = toRadians(lon1 - lon2);
+    public Double minimumCost()
 
-        Double a = Math.sin(DeltaLat / 2) * Math.sin(DeltaLat / 2) +
+
+    //Computes the straight-line distance between two pairs of lat/lon using Haversine formula.
+    /*
+    public Double haversineFormula(Double lat1, Double lon1, Double lat2, Double lon2){
+        final int R = 6371; //Earth's radius in kilometers.
+        Double dLat = toRadians(lat1 - lat2);
+        Double dLon = toRadians(lon1 - lon2);
+
+        Double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
                     Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2))
-                            * Math.sin(DeltaLon / 2) * Math.sin(DeltaLon / 2);
+                            * Math.sin(dLon / 2) * Math.sin(dLon / 2);
         Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         return R * c; //Returns d
     }
+    */
 
-    public Double toRadians(Double val){
-        return val * Math.PI / 180;
+
+    public float haversineFormula(OSMNode start, OSMNode target){
+        final int R = 6371; //Earth's radius in kilometers.
+        float dLat = toRadians(start.getLat() - target.getLat());
+        float dLon = toRadians(start.getLon() - target.getLon());
+
+        float a = (float)Math.sin(dLat / 2) * (float)Math.sin(dLat / 2) +
+                (float)Math.cos(toRadians(start.getLat())) * (float)Math.cos(toRadians(target.getLat()))
+                        * (float)Math.sin(dLon / 2) * (float)Math.sin(dLon / 2);
+
+        float c = 2 * (float)Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return R * c; //Returns d
+    }
+
+    public float toRadians(float val){
+        return val * (float)Math.PI / 180;
     }
 
 }
