5ff323c2b8f9f108f2880bdd8ca69929a7de73c6
Adam Hadou Temsamani <ahad@itu.dk>
2022-04-22 11:46:50 +0200

    Initial

--- Map of Denmark/app/src/main/java/bfst22/vector/DijkstraSP.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/DijkstraSP.java
@@ -1,39 +1,114 @@
 package bfst22.vector;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class DijkstraSP {
+    Map<PolyPoint, Double> distanceMap;
+    Map<PolyPoint, PolyPoint> edgeMap;
+
+
     private double[] distTo;
     private Edge[] edgeTo;
-    private PriorityQueue<Double> pq;
+    private IndexMinPQ<Double> pq;
+    List<PolyPoint> nodes;
+    Graph G = new Graph(nodes);
 
-    public DijkstraSP(Graph G, int s){
-        distTo = new double[G.getV()];
-        edgeTo = new Edge[G.getV()];
-        for(int i = 0; i < G.getV(); i++){
-            distTo[i] = Double.POSITIVE_INFINITY;
-            distTo[s] = 0.0;
 
-            pq = new PriorityQueue<>(G.getV());
-            pq.add(distTo[s]);
-            while(!pq.isEmpty()) {
+    public DijkstraSP(Graph G, PolyPoint start, PolyPoint target) {
+        distanceMap = new HashMap<>();
+        edgeMap = new HashMap<>();
+        edgeTo = new Edge[G.getVertex()];
+
+        for (int i = 0; i < G.getVertex(); i++) {
+            distanceMap.put(nodes.get(i), Double.POSITIVE_INFINITY);
+            distanceMap.put(start, 0.0);
+        }
+
+        pq = new IndexMinPQ<>(G.getVertex());
+        pq.insert(G.indexes.get(start), distanceMap.get(start));
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            for (Edge e : G.list) {
+                relax(e, target);
             }
         }
     }
 
-    private void relax(Edge e){
+    float h(PolyPoint start, PolyPoint target) { //Start of A*
+        Distance d = new Distance();
+        return d.haversineFormula(start, target);
+
+    }
+
+    public void relax(Edge e, PolyPoint target) {
+        int v = G.indexes.get(e.getFrom());
+        int w = G.indexes.get(e.getTo());
+        if (distanceMap.get(e.getTo()) > distanceMap.get(e.getFrom()) + e.getWeight()) {
+            distanceMap.replace(e.getTo(), distanceMap.get(e.getFrom()) + e.getWeight());
+            edgeTo[w] = e;
+            double priority = distanceMap.get(e.getTo()) + h(e.getTo(), e.getFrom());
+            if (pq.contains(w)) {
+                pq.decreaseKey(w, priority);
+            } else {
+                pq.insert(w, priority);
+            }
+        }
+    }
+
+    public double distTo(PolyPoint v) {
+        return distanceMap.get(v);
+    }
+
+    public Iterable<Edge> pathTo(Edge e) {
+        if (!hasPathTo(edgeMap.get(e.getFrom()))) return null;
+        Stack<Edge> path = new Stack<>();
+        for (Edge i = edgeTo[G.indexes.get(e.getFrom())]; i != null; i = edgeTo[G.indexes.get(e.getFrom())]) {
+            path.push(e);
+        }
+        return path;
+    }
+
+
+    public boolean hasPathTo(PolyPoint v) {
+        return distanceMap.get(v) < Double.POSITIVE_INFINITY;
+    }
+}
+
+    /* Integer implementation
+    public DijkstraSP(Graph G, int s, int t){
+        distTo = new double[G.getVertex()];
+        edgeTo = new Edge[G.getEdge()];
+        for(int i = 0; i < G.getVertex(); i++){
+            distTo[i] = Double.POSITIVE_INFINITY;
+            distTo[s] = 0.0;
+
+            pq = new IndexMinPQ<>(G.getVertex());
+            pq.insert(s, distTo[s]);
+            while(!pq.isEmpty()) {
+                int v = pq.delMin();
+                for(Edge e : G.adj(v)) {
+                    relax(e, t);
+                }
+            }
+        }
+    }
+    */
+
+    /*
+    private void relax(Edge e, int target){
         int v = e.getFrom();
         int w = e.getTo();
         if(distTo[w] > distTo[v] + e.getWeight()){
             distTo[w] = distTo[v] + e.getWeight();
             edgeTo[w] = e;
+            double priority = distTo[w] + h(w,target);
+            if(pq.contains(w)){
+                    pq.decreaseKey(w,priority);
+            } else{
+                pq.insert(w,priority);
+            }
 
         }
     }
-
-    public double getDistTo(int v){
-        return distTo[v];
-    }
+    */
 
 
-
-}
--- Map of Denmark/app/src/main/java/bfst22/vector/Graph.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/Graph.java
@@ -1,11 +1,70 @@
 package bfst22.vector;
-//Not done yet
-public class Graph {
-    private final int V;
-    private int E;
-    private Bag<Edge>[] adj; //Maybe should implement bag, unsure at the moment
 
+//Not done yet
+
+import java.util.*;
+
+public class Graph {
+    private int V;
+    private int E;
+    float speedlimit = 0;
+    Map<PolyPoint, LinkedList<Edge>> adjList = new HashMap<>();
+    Map<PolyPoint, Integer> indexes = new HashMap<>();
+    LinkedList<Edge> list = new LinkedList<>();
+    int index = -1;
+
+    //Map<PolyPoint, TreeMap<PolyPoint,Float>> adj = new HashMap<>();
+
+    public Graph(List<PolyPoint> nodes){
+        for(int i = 0; i < nodes.size(); i++){
+            PolyPoint node = nodes.get(i);
+            adjList.put(node,list);
+            indexes.put(node,++index);
+        }
+    }
+
+    public void addEdge(PolyPoint from, PolyPoint to, float weight){
+        LinkedList<Edge> list = new LinkedList<>();
+        list.add(new Edge(from, to, setWeight(from,to, speedlimit)));
+
+    }
+
+
+    /*
+    public void addEdge(PolyPoint from, PolyPoint to, float weight){
+
+    }
+
+    */
+    public void setNodecount(int nodecount){
+        if(V == 0) V = nodecount;
+    }
+
+    public int getVertex(){
+        return V;
+    }
+
+    public int getEdge(){
+        return E;
+    }
+
+    public void setWaycount(int waycount){
+        if(E == 0) E = waycount;
+    }
+
+    public float setWeight(PolyPoint from, PolyPoint to, float speedlimit){
+        Distance d = new Distance();
+        return d.haversineFormula(from,to)/speedlimit;
+    }
+
+    /*
+    //private Bag<PolyPoint>[] adj;
     private int[] indegree;
+    PolyLine way;
+    int speedLimit;
+    private PolyPoint from;
+    private PolyPoint to;
+    private float weight;
 
     public Graph(int V){
         if (V < 0) throw new IllegalArgumentException("error");
@@ -19,16 +78,37 @@ public class Graph {
     }
 
     public Graph(int V, int E){
-        this(V); //Unsure what this does atm
-        if(E < 0) throw new IllegalArgumentException("Non-negative edge");
+        this(V); //Calls previous Graph constructor
             for(int i = 0; i < E; i++){
-                //Finde en måde at indlæse v, w og weight
-                int from = 0;
-                int to = 0;
-                double weight = 0.0;
+
                 Edge e = new Edge(from, to, weight);
             }
 
+    }
+
+ */
+    /*
+    public Graph(List<PolyLine> nodes, int V){
+        this.vertex = vertex;
+        this.edge = 0;
+        indegree = new int[vertex]
+        adj = new Bag[vertex];
+            for(int i = 0; i < vertex; i++){
+                adj[i] = new Bag<Edge>();
+            }
+    }
+
+    public Graph(List<PolyLine> ways,int V,int E){
+        this(ways,V);
+            int i = 0;
+            for(PolyLine w : ways){
+                from.lat = way.coords[i++];
+                from.lon = way.coords[i++];
+                to.lat = way.coords[i++];
+                to.lon = way.coords[i++];
+                weight = setWeight(from, to, speedLimit);
+                Edge e = new Edge(from, to , weight);
+            }
     }
 
     public Iterable<Edge> adj(int v){
@@ -37,7 +117,7 @@ public class Graph {
 
     public Iterable<Edge> edges() {
         Bag<Edge> list = new Bag<Edge>();
-        for(int i = 0; i < V; i++){
+        for(int i = 0; i < vertex; i++){
             for(Edge e : adj(i)){
                 list.add(e);
             }
@@ -46,23 +126,37 @@ public class Graph {
     }
 
     public int getV(){
-        return V;
+        return vertex;
+    }
+    public int getE() { return edge; }
+
+    public void setNodecount(int nodecount){
+        if(vertex == 0) vertex = nodecount;
     }
 
-    public int getE(){
-        return E;
+    public void setWaycount(int waycount){
+        if(edge == 0) edge = waycount;
+    }
+
+    public float setWeight(PolyPoint from, PolyPoint to, int speedlimit){
+        Distance d = new Distance();
+        return d.haversineFormula(from,to)/speedlimit;
     }
 
     public void addEdge(Edge e){
-        int from = e.getFrom();
-        int to = e.getTo();
+        PolyPoint from = e.getFrom();
+        PolyPoint to = e.getTo();
         adj[from].add(e);
         indegree[to]++;
-        E++;
     }
 
     public int getIndegree(int v){
         return indegree[v];
     }
 
+    public void setSpeedLimit(int limit){
+        speedLimit = limit;
+    }
+
+    */
 }
