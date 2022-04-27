3319cd0ddff68da5256f204f07e8df4d538a1257
Adam Hadou Temsamani <ahad@itu.dk>
2022-04-27 16:15:05 +0200

    Direction.java + Further progress with DijkstraSPTest

--- Map of Denmark/app/src/main/java/bfst22/vector/Directions.java
+++ Map of Denmark/app/src/main/java/bfst22/vector/Directions.java
@@ -1,5 +1,33 @@
 package bfst22.vector;
 
 public class Directions {
+    Distance d = new Distance();
+    Address adress;
+
+    /* Returns a String with the description of the next turn */
+    public String turn(PolyPoint start, PolyPoint to){
+            float angle = getAngle(start, to);
+
+            /*Driving Forward*/
+            if (angle > 45 || angle < 135) {
+                return "Continue along " + adress.street + "for " + d.haversineFormula(start, to) + " m";
+            }
+            /*Right Turn*/
+            if (angle < 45 || angle > 315) {
+                return "Turn right onto " + adress.street + "for " + d.haversineFormula(start, to) + " m";
+            }
+            /*Left Turn*/
+            if(angle > 135 || angle < 225){
+                return "Turn left onto " + adress.street + "for " + d.haversineFormula(start, to) + " m";
+            }
+            else return null; //Ved ikke lige hvad ellers man kan return for nu
+    }
+
+    /* Returns the angle between two nodes (Start of the road to the end of the road) */
+    public float getAngle(PolyPoint from, PolyPoint to){
+        float angle = (float) Math.toDegrees(Math.atan2(to.lat - from.lat,to.lon - from.lon));
+
+        return angle;
+    }
 
 }
--- Map of Denmark/app/src/test/java/bfst22/vector/DijkstraSPTest.java
+++ Map of Denmark/app/src/test/java/bfst22/vector/DijkstraSPTest.java
@@ -1,17 +1,18 @@
 package bfst22.vector;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.testng.annotations.Test;
 import java.util.*;
-import bfst22.vector.*;
 
 public class DijkstraSPTest {
     //ArrayList<PolyPoint> list = new ArrayList<>(); ArrayList that will hold all the PolyPoints in the Graph.
-
+    List<PolyPoint> nodes = new ArrayList<>();
+    Graph g = new Graph(nodes);
+    VehicleType vehicleType = VehicleType.MOTORCAR;
+    Distance d = new Distance();
     /*PolyPoint objects*/
-        /*
         PolyPoint A = new PolyPoint(1,12,3);
         PolyPoint B = new PolyPoint(2, 10,4);
         PolyPoint C = new PolyPoint(3,8,6);
@@ -23,24 +24,19 @@ public class DijkstraSPTest {
         PolyPoint I = new PolyPoint(9, 5,3);
         PolyPoint J = new PolyPoint(10, 5,6);
         PolyPoint K = new PolyPoint(11, 4,9);
-        */
 
         /*Method that turns an ArrayList of PolyPoints
-        ArrayList<PolyPoint> returnArraylist(){
-
-            list.add(new PolyPoint(1,12,3));
-            list.add(new PolyPoint(2, 10,4));
-            list.add(new PolyPoint(3,8,6));
-            list.add(new PolyPoint(4,7,8));
-            list.add(new PolyPoint(5, 5,10));
-            list.add(new PolyPoint(6,10,2));
-            list.add(new PolyPoint(7,7,2));
-            list.add(new PolyPoint(8, 7,4));
-            list.add(new PolyPoint(9, 5,3));
-            list.add(new PolyPoint(10, 5,6));
-            list.add(new PolyPoint(11,4,9));
-            return list;
-        }
+        nodes.add(new PolyPoint(0,12,3)); //A
+        nodes.add(new PolyPoint(1, 10,4)); //B
+        nodes.add(new PolyPoint(2,8,6)); //C
+        nodes.add(new PolyPoint(3,7,8)); //D
+        nodes.add(new PolyPoint(4, 5,10)); //E
+        nodes.add(new PolyPoint(5,10,2)); //F
+        nodes.add(new PolyPoint(6,7,2)); //G
+        nodes.add(new PolyPoint(7, 7,4)); //H
+        nodes.add(new PolyPoint(8, 5,3)); //I
+        nodes.add(new PolyPoint(9, 5,6)); //J
+        nodes.add(new PolyPoint(10,4,9)); //K
         */
 
     @BeforeEach void setUp(){
@@ -48,44 +44,40 @@ public class DijkstraSPTest {
 
     }
 
-    @Test void DijkstraTest(){
-        VehicleType vehicleType = VehicleType.MOTORCAR;
-        Distance d = new Distance();
-        ArrayList<PolyPoint> list = new ArrayList<>();
+    @Test void dijkstraTest(){
+        nodes.add(A);
+        nodes.add(B);
+        nodes.add(C);
+        nodes.add(D);
+        nodes.add(E);
+        nodes.add(F);
+        nodes.add(G);
+        nodes.add(H);
+        nodes.add(I);
+        nodes.add(J);
+        nodes.add(K);
 
-        list.add(new PolyPoint(0,12,3)); //A
-        list.add(new PolyPoint(1, 10,4)); //B
-        list.add(new PolyPoint(2,8,6)); //C
-        list.add(new PolyPoint(3,7,8)); //D
-        list.add(new PolyPoint(4, 5,10)); //E
-        list.add(new PolyPoint(5,10,2)); //F
-        list.add(new PolyPoint(6,7,2)); //G
-        list.add(new PolyPoint(7, 7,4)); //H
-        list.add(new PolyPoint(8, 5,3)); //I
-        list.add(new PolyPoint(9, 5,6)); //J
-        list.add(new PolyPoint(10,4,9)); //K
+        g.addEdge(nodes.get(0), nodes.get(1),d.haversineFormula(nodes.get(0), nodes.get(1)));
+        g.addEdge(nodes.get(1), nodes.get(2),d.haversineFormula(nodes.get(1), nodes.get(2)));
+        g.addEdge(nodes.get(2), nodes.get(3),d.haversineFormula(nodes.get(2), nodes.get(3)));
+        g.addEdge(nodes.get(3), nodes.get(4),d.haversineFormula(nodes.get(3), nodes.get(4)));
+        g.addEdge(nodes.get(0), nodes.get(5),d.haversineFormula(nodes.get(0), nodes.get(5)));
+        g.addEdge(nodes.get(5), nodes.get(6),d.haversineFormula(nodes.get(5), nodes.get(6)));
+        g.addEdge(nodes.get(6), nodes.get(7),d.haversineFormula(nodes.get(6), nodes.get(7)));
+        g.addEdge(nodes.get(0), nodes.get(3),d.haversineFormula(nodes.get(0), nodes.get(3)));
+        g.addEdge(nodes.get(0), nodes.get(7),d.haversineFormula(nodes.get(0), nodes.get(7)));
+        g.addEdge(nodes.get(6), nodes.get(8),d.haversineFormula(nodes.get(6), nodes.get(8)));
+        g.addEdge(nodes.get(6), nodes.get(9),d.haversineFormula(nodes.get(6), nodes.get(9)));
+        g.addEdge(nodes.get(9), nodes.get(10),d.haversineFormula(nodes.get(9), nodes.get(10)));
+        g.addEdge(nodes.get(8), nodes.get(9),d.haversineFormula(nodes.get(8), nodes.get(9)));
+        g.addEdge(nodes.get(10), nodes.get(4),d.haversineFormula(nodes.get(10), nodes.get(4)));
+        g.addEdge(nodes.get(7), nodes.get(4),d.haversineFormula(nodes.get(7), nodes.get(4)));
+        g.addEdge(nodes.get(7), nodes.get(2),d.haversineFormula(nodes.get(7), nodes.get(2)));
 
-        Graph g = new Graph(list);
-        g.addEdge(list.get(0),list.get(1),d.haversineFormula(list.get(0),list.get(1)));
-        g.addEdge(list.get(1),list.get(2),d.haversineFormula(list.get(1),list.get(2)));
-        g.addEdge(list.get(2),list.get(3),d.haversineFormula(list.get(2),list.get(3)));
-        g.addEdge(list.get(3),list.get(4),d.haversineFormula(list.get(3),list.get(4)));
-        g.addEdge(list.get(0),list.get(5),d.haversineFormula(list.get(0),list.get(5)));
-        g.addEdge(list.get(5),list.get(6),d.haversineFormula(list.get(5),list.get(6)));
-        g.addEdge(list.get(6),list.get(7),d.haversineFormula(list.get(6),list.get(7)));
-        g.addEdge(list.get(0),list.get(3),d.haversineFormula(list.get(0),list.get(3)));
-        g.addEdge(list.get(0),list.get(7),d.haversineFormula(list.get(0),list.get(7)));
-        g.addEdge(list.get(6),list.get(8),d.haversineFormula(list.get(6),list.get(8)));
-        g.addEdge(list.get(6),list.get(9),d.haversineFormula(list.get(6),list.get(9)));
-        g.addEdge(list.get(9),list.get(10),d.haversineFormula(list.get(9),list.get(10)));
-        g.addEdge(list.get(8),list.get(9),d.haversineFormula(list.get(8),list.get(9)));
-        g.addEdge(list.get(10),list.get(4),d.haversineFormula(list.get(10),list.get(4)));
-        g.addEdge(list.get(7),list.get(4),d.haversineFormula(list.get(7),list.get(4)));
-        g.addEdge(list.get(7),list.get(2),d.haversineFormula(list.get(7),list.get(2)));
-
-        DijkstraSP sp = new DijkstraSP(g,list.get(0), list.get(10), vehicleType);
-        System.out.println(sp.pathTo(list.get(10)));
+        DijkstraSP sp = new DijkstraSP(g, nodes.get(0), nodes.get(10), vehicleType);
+        assertEquals("0->7  5,10 7->4  6,32 4->10 1.44 ",sp.pathTo(nodes.get(10)).toString());
     }
+
 
     @AfterEach void tearDown(){
 
