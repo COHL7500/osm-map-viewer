8ea1e98e6e624055f33d71e78839690f294f2642
Weihao Chen Nyholm-Andersen <weny@itu.dk>
2022-04-29 15:19:13 +0200

    Improving search suggestions

---
+++ Map of Denmark/app/src/main/java/bfst22/vector/AutoComplete.java
@@ -0,0 +1,23 @@
+package bfst22.vector;
+
+import java.beans.EventHandler;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Side;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+// Code from: https://gist.github.com/floralvikings/10290131
+
+public class AutoComplete extends TextField {
+    
+}
