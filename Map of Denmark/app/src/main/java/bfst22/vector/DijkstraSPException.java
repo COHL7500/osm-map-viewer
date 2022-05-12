cfa67997c702361b8d980e7c0c19c6f9d3e016c3
COHL7500 <cohl7500@gmail.com>
2022-05-12 15:09:47 +0200

    Remove DijkstraSPException.java

--- Map of Denmark/app/src/main/java/bfst22/vector/DijkstraSPException.java
+++
@@ -1,8 +0,0 @@
-package bfst22.vector;
-
-/* If we want to implement an Exception */
-public class DijkstraSPException extends RuntimeException{
-    public DijkstraSPException(String message){
-        super(message);
-    }
-}
